51619ae82923aa76d324ca6975ae825739cf5715
gizem-ucar <77545358+gizem-ucar@users.noreply.github.com>
2021-02-12 23:20:21 +0300

    12.02.2021

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -8,7 +8,7 @@ using System.Text;
 
 namespace Business.Concrete
 {
-    internal class CarManager : ICarService
+    public class CarManager : ICarService
     {
         ICarDal _carDal;
         public CarManager(ICarDal carDal)
