acde321d97480f5611d46d92d134322a654ea773
gizem-ucar <77545358+gizem-ucar@users.noreply.github.com>
2021-03-29 01:32:51 +0300

    Update IUserService.cs

--- Business/Abstract/IUserService.cs
+++ Business/Abstract/IUserService.cs
@@ -1,14 +1,17 @@
-﻿using Core.Utilities.Results;
-using Entities.Concrete;
-using System;
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Results;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Business.Abstract
 {
     public interface IUserService
     {
+        IResult Add(User user);
+        IResult Update(User user);
+        IResult Delete(User user);
         IDataResult<List<User>> GetAll();
         IDataResult<User> GetById(int userId);
+        List<OperationClaim> GetClaims(User user);
+        User GetByMail(string email);
     }
 }
