670656dd6104362d8a18e4dc60c7c66f9d982091
gizem-ucar <77545358+gizem-ucar@users.noreply.github.com>
2021-03-29 00:31:18 +0300

    Create CarValidator.cs
    29.03

---
+++ Core/Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.BrandName).NotEmpty();
+            RuleFor(c=>c.BrandName).MinimumLength(2);
+        }
+    }
+}
