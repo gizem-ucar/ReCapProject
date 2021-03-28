2cae92559842b66bcc78e22e6912f161f16410cd
gizem-ucar <77545358+gizem-ucar@users.noreply.github.com>
2021-03-29 00:32:58 +0300

    Create EfEntityRepositoryBase.cs
    29.03

---
+++ Core/Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,65 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity,TContext>:IEntityRepository<TEntity>
+        where TEntity: class, IEntity, new()
+        where TContext: DbContext,new()
+    {
+        public void Add(TEntity entity)
+        {
+            using (TContext context = new TContext())                      //using içinde yazmamızın sebebi: context pahalı ve using ile çöp toplayıcıyı bekleme 
+                                                                           //ve bittiği an  dispose et.   dispose: bellekten uçur demek.
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();                                     //Bunu yazınca ekliyor.
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)   //tek data getirecek
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null                              //filter null ise 
+                    ? context.Set<TEntity>().ToList()                  //evetse bu
+                    : context.Set<TEntity>().Where(filter).ToList();   //hayırsa
+            }
+        }
+
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
