d0b9408b8547ee474dc1e8ce1a47ed937b63a4ff
gizem-ucar <77545358+gizem-ucar@users.noreply.github.com>
2021-03-29 00:44:29 +0300

    Update UsersController.cs

--- WebAPI/Controllers/UsersController.cs
+++ WebAPI/Controllers/UsersController.cs
@@ -1,9 +1,11 @@
-﻿using Business.Abstract;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Business.Abstract;
+using Core.Entities.Concrete;
 
 namespace WebAPI.Controllers
 {
@@ -17,7 +19,7 @@ namespace WebAPI.Controllers
             _userService = userService;
         }
 
-        [HttpGet("getal")]
+        [HttpGet]
         public IActionResult GetAll()
         {
             var result = _userService.GetAll();
@@ -28,16 +30,48 @@ namespace WebAPI.Controllers
             return BadRequest(result);
         }
 
-        [HttpGet("getbyid")]
-        public IActionResult GetById(int userId)
+        [HttpGet("id")]
+        public IActionResult GetById(int id)
         {
-            var result = _userService.GetById(userId);
+            var result = _userService.GetById(id);
             if (result.Success)
             {
                 return Ok(result);
             }
             return BadRequest(result);
+        }
 
+        [HttpPost("add")]
+        public IActionResult Add(User user)
+        {
+            var result = _userService.Add(user);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(User user)
+        {
+            var result = _userService.Delete(user);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(User user)
+        {
+            var result = _userService.Update(user);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
         }
     }
 }
