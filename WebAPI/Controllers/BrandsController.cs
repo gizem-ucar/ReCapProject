594e4f74282cbce8d8ebc02faed28b8bd334a2d1
gizem-ucar <77545358+gizem-ucar@users.noreply.github.com>
2021-03-29 00:44:43 +0300

    Update BrandsController.cs

--- WebAPI/Controllers/BrandsController.cs
+++ WebAPI/Controllers/BrandsController.cs
@@ -8,37 +8,74 @@ using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
 {
-    [Route("api/[controller]")]  
+    [Route("api/[controller]")]
     [ApiController]
-    public class BrandsController:ControllerBase
+    public class BrandsController : ControllerBase
     {
+
         IBrandService _brandService;
         public BrandsController(IBrandService brandService)
         {
             _brandService = brandService;
         }
 
-        [HttpGet("getal")]
+        [HttpPost("add")]
+        public IActionResult Add(Brand brand)
+        {
+            var result = _brandService.Add(brand);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete(Brand brand)
+        {
+            var result = _brandService.Delete(brand);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update(Brand brand)
+        {
+            var result = _brandService.Update(brand);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet("getall")]
         public IActionResult GetAll()
         {
+            //Thread.Sleep(2000);
             var result = _brandService.GetAll();
             if (result.Success)
             {
                 return Ok(result);
             }
-            return BadRequest(result);
+            return BadRequest(result.Message);
         }
 
         [HttpGet("getbyid")]
-        public IActionResult GetById(int brandId)
+        public IActionResult GetById(int id)
         {
-            var result = _brandService.GetById(brandId);
+            var result = _brandService.GetById(id);
             if (result.Success)
             {
                 return Ok(result);
             }
             return BadRequest(result);
-
         }
+
+
     }
+
 }
